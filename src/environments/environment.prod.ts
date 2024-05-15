e46c6bcc5884c46dbe2cd042485840d25ec2c561
mmesrar <m_mesrar@etu.enset-media.ac.ma>
2024-05-15 20:04:44 +0100

    change apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiUrl : "http://51.20.53.50:3000"
+  apiUrl : "https://api.mcovery.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl : "http://51.20.53.50:3000"
+  apiUrl : "https://api.mcovery.com"
   //apiUrl : "http://localhost:3000"
 };
 
